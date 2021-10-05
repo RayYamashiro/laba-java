de29705feafe93a079300211b4f4d46b0bcffefd
RayYamashiro <vlad_alferov02@mail.ru>
2021-10-05 21:24:16 +0300

    переделал пакет

--- src/Program.java
+++ src/Program.java
@@ -5,7 +5,7 @@ public class Program
     public static void main (String [] args)
     {
       int a = 7;
-        Vector<String> vec_tor = new Vector<String>();
+        Vector<String> vec_tor = new Vector();
         vec_tor.add("Geeks");
         vec_tor.addElement("Last");
     }
